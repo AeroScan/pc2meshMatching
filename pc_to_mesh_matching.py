a619b4f672645e90fdb1c1216e88775ac17c4f28
Igor Maurell <igormaurell@gmail.com>
2022-06-13 12:36:00 -0300

    adding argparse

--- pc_to_mesh_matching.py
+++ pc_to_mesh_matching.py
@@ -2,39 +2,44 @@ import numpy as np
 import open3d as o3d
 from textual2pcd import write_pcd_by_pc
 
-def writeTXT(filename, points, normals, labels):
-    pcd = np.zeros((points.shape[0], 8))
-    pcd[:,0:3] = points
-    pcd[:, 3:6] = normals
-    pcd[:, 7] = labels
+import argparse
 
-    np.savetxt(filename, pcd, fmt='%f') 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Perform a matching of a point cloud and a mesh, saving as PCD using the field \'lable\' to identify the triangle id for each point.')
+    parser.add_argument('input_filename_pc', type=str, help='input filename of point cloud in .pcd.')
+    parser.add_argument('input_filename_mesh', type=str, help='input filename of mesh in .obj.')
+    parser.add_argument('output_filename', type=str, help='output filename in .pcd.')
+    parser.add_argument('-b', '--binary', action='store_true', help='save in binary format. Default = False')
+    args = vars(parser.parse_args())
 
-scene = o3d.t.geometry.RaycastingScene()
+    input_filename_pc = args['input_filename_pc']
+    input_filename_mesh = args['input_filename_mesh']
+    output_filename = args['output_filename']
+    binary = args['binary']
 
-mesh = o3d.t.geometry.TriangleMesh.from_legacy(o3d.io.read_triangle_mesh("70_10.obj"))
+    scene = o3d.t.geometry.RaycastingScene()
+    mesh = o3d.t.geometry.TriangleMesh.from_legacy(o3d.io.read_triangle_mesh(input_filename_mesh))
+    scene.add_triangles(mesh)
 
-scene.add_triangles(mesh)
+    pcd = o3d.io.read_point_cloud(input_filename_pc)
 
-pcd = o3d.io.read_point_cloud("70_10.pcd")
+    ans = scene.compute_closest_points(o3d.core.Tensor(np.asarray(pcd.points), dtype=o3d.core.Dtype.Float32))
 
-ans = scene.compute_closest_points(o3d.core.Tensor(np.asarray(pcd.points), dtype=o3d.core.Dtype.Float32))
+    points = np.asarray(pcd.points)
+    aprox_points = ans['points'].numpy()
+    mean_dist = np.mean(np.sqrt(np.power(points - aprox_points, 2)))
 
-points = np.asarray(pcd.points)
-aprox_points = ans['points'].numpy()
-mean_dist = np.mean(np.sqrt(np.power(points - aprox_points, 2)))
+    invalid = 0
+    for elem in ans['primitive_ids'].numpy():
+        if elem == o3d.t.geometry.RaycastingScene.INVALID_ID:
+            invalid += 1
 
-invalid = 0
-for elem in ans['primitive_ids'].numpy():
-    if elem == o3d.t.geometry.RaycastingScene.INVALID_ID:
-        invalid += 1
+    print('Mesh to Points mean distance:', mean_dist)
+    print('No collision points:', invalid)
 
-print('Mesh to Points mean distance:', mean_dist)
-print('No collision points:', invalid)
+    pc = np.zeros((points.shape[0], 7))
+    pc[:,0:3] = points
+    pc[:, 3:6] = np.asarray(pcd.normals)
+    pc[:, 6] = ans['primitive_ids'].numpy()
 
-pc = np.zeros((points.shape[0], 7))
-pc[:,0:3] = points
-pc[:, 3:6] = np.asarray(pcd.normals)
-pc[:, 6] = ans['primitive_ids'].numpy()
-
-write_pcd_by_pc(pc, '70_10_match.pcd', ['x','y','z','normal_x','normal_y','normal_z','label'], binary=False)
+    write_pcd_by_pc(pc, output_filename, ['x','y','z','normal_x','normal_y','normal_z','label'], binary=binary)
